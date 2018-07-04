67b256e3366e3ade83210e98c43920fa054d49f9
Kevin Aquino <kevin.aquino@monash.edu>
2018-07-04 11:03:16 +1000

    adding in the global hopf model

--- run_network_model.m
+++ run_network_model.m
@@ -4,7 +4,7 @@
 
 % Outputs that have to be saved per G (would be good to keep it quite standard)
 % The FC matrix 
-% The FCD matrix
+% The FCD KS-estimate
 % The estimated time series as well (which is needed)
 
 function run_model(sc_matrix,time_series,G,model)
@@ -17,16 +17,21 @@ function run_model(sc_matrix,time_series,G,model)
 
 switch model	
 	case 'HOPF+GLOBAL',
-		% Here it is the hopf global model i.e. setting a=0.
-		
+		% Here it is the hopf global model i.e. setting a=-0.01.
+		folder = 'results/HOPF+GOBAL';
+		[ts_simulated_all,grandFCcorr,bifpar] = run_hopf_model_homogenous_bif(sc_matrix,time_series,G,folder);
 	case 'DECO+WANG',
 		% Here look at the model what does this use? not the balanced EI model (will have to look)
 	case 'BTF',
 		% Here will have to grab the BTF model and then look at stuff
 	case 'HOPF+HETEROGENOUS',
 		% Here look at the heterogenous 
+		folder = 'results/HOPF+HETEROGENOUS';
+		[FC_simul, FC_emp, fitting, meta, ksP, PhasesD, Phases, bifpar] = run_hopf_model_heterogenous_bif(sc_matrix,time_series,G,folder);
+		% Will have to instead have something more uniform, but for now keep this structure.
 	case 'DECO+WANG+BALANCED',
 		% Here it is the balanced EI model which is essentially the noise degree model
+		%  -- This is esentiall the noise-degree model but for now keep it seperate
 	case 'HOPF+ANEC',
 		% Here it is the HOPF model with ANEC
 	case 'NOISY+DEGREE',
