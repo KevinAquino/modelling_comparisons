ffe2797a85c2e7c73f1247dec7633c12bdc531d2
Kevin Aquino <kevin.aquino@monash.edu>
2018-07-19 18:42:27 +1000

    added more flags to work out what is going on

--- models/run_BTF_model.m
+++ models/run_BTF_model.m
@@ -1,4 +1,4 @@
-function run_BTF_model(C,cparam);
+function run_BTF_model(C,cparam)
 
 
 % Function here to solve BTF, solve it over a long time
@@ -11,12 +11,23 @@ sys = BTF2003(C);
 sys.pardef=bdSetValue(sys.pardef,'C',cparam);
 % keyboard
 
-% Solve the model for 2 seconds
+% Solve the model for 40 seconds to remove transients
+sol = bdSolve(sys,[0 40000]);
+% for j=1:size(C,1)*3,
+% 	y_all(j,:) = interp1(sol.x,sol.y(j,:),[1:1:40000]);
+% end
+
+% keyboard
+Y = reshape(sol.y(:,end),size(C,1),3,1);
+for j=1:3,	
+	sys.vardef(j).value = Y(:,j);
+end
+
+% After this, let the system run.
 sol = bdSolve(sys,[0 2000]);
 for j=1:size(C,1)*3,
 	y_all(j,:) = interp1(sol.x,sol.y(j,:),[1:1:2000]);
 end
-
 
 Y = reshape(y_all,size(C,1),3,2000);
 
@@ -36,12 +47,12 @@ end
 Vin = inputNeural;
 
 % keyboard;
-
+Vtin = [];
 % W = squeeze(Y(:,2,1:2500:end));
 % Z = squeeze(Y(:,3,1:2500:end));
 
 % Solve this for 147*2 more seconds;
-for n=1:147,	
+for n=1:200,	
 	disp(['Iteration: ',num2str(n),'....']);
 	% Now set the last point as the intial value for the simulation to continue the simulation
 	sys2 = sys;
@@ -64,11 +75,12 @@ for n=1:147,
 
 	for n=1:68,
 		% inputNeural(n,:) = movmean(abs(gradient(Vtest(n,:),0.1)),2500);
-		inputNeural(n,:) = abs(gradient(Vtest(n,:),0.1));
+		inputNeural(n,:) = abs(gradient(Vtest(n,:),1));
 		% inputNeural(n,:) = filtfilt(bfilt2,afilt2,(gradient(Vtest(n,:),0.1)));
 	end
 
 	Vin = [Vin,inputNeural];
+	Vtin = [Vtin,Vtest];
 	lastValues = Y(:,:,end);
 
 	
@@ -81,36 +93,49 @@ end
 
 % dt = 2000/20000*250/1000;
 % 1 ms
-% dt = 1;
+dt = 1;
 
 % time = 0:dt:(size(Vin,2)-1)*dt;
 % time_sampled = time(1:250:end);
-% time = 0:dt:(size(Vin,2)-1)*dt;
+time = 0:dt:(size(Vin,2)-1)*dt;
 
 
 % % Now model the HRF with a standard two-gamma distribution
-% modelHrf = gampdf(time/1e3, 6, 1) - gampdf(time/1e3, 16, 1)/6;		
-% hrf = circshift(modelHrf.',round(length(modelHrf)/2)).';
+modelHrf = gampdf(time/1e3, 6, 1) - gampdf(time/1e3, 16, 1)/6;		
+hrf = circshift(modelHrf.',round(length(modelHrf)/2)).';
 % % keyboard;
-% TRs = 0:2:40*2;
+% TRs = 0:0.01:100*4;
 % % Convolve the BOLD response 
-% for n=1:68,
-% 	BOLD(n,:) = conv(Vin(n,:),hrf,'same');
-% 	BOLD_interp(n,:) = interp1(time/1e3,BOLD(n,:),TRs);
-% end
+for n=1:68,
+	BOLD(n,:) = conv(Vin(n,:),hrf,'same');
+% 	BOLD_interp(n,:) = interp1(time(1e5:end)/1e3,zscore(BOLD(n,1e5:end)),TRs);
+end
 
+keyboard;
+TRs = 0:0.01:100*4;
+% % Downsample the BOLD response 
+for n=1:68,	
+	BOLD_interp(n,:) = interp1(time(1e5:end)/1e3,zscore(BOLD(n,1e5:end)),TRs);
+end
 
+gs = mean(BOLD_interp);
+% keyboard
+noNans = find(~isnan(gs));
+
+% keyboard;
 % BOLD_interp(isnan(BOLD_interp)) = 0;
 
 % BOLD = BOLD_interp;
 
 % bb = BOLD_interp(:,14:end);
-
-for n=1:68,b(n,:) = BOLD(200,Vin(n,:));end
-zt = b(:,1e5:end);
-zt = zscore(zt,[],2);
-figure;
-imagesc(corr(zt.'));
+% 
+% for n=1:68,b(n,:) = BOLD(200,Vin(n,:));end
+% zt = b(:,1e5:end);
+% zt = zscore(zt,[],2);
+% figure;
+% imagesc(corr(zt.'));
+figure;imagesc(corr(BOLD_interp(:,noNans).'))
 caxis([0 1]);
 title(['G = ',num2str(cparam/0.2)]);
 axis image;
+drawnow;
