56f2c27054c2e68052a4acd73e6e3388731f42f8
Kevin Aquino <kevin.aquino@monash.edu>
2018-10-27 02:39:02 +1100

    fixed an error

--- run_multiple_BTF.m
+++ run_multiple_BTF.m
@@ -1,6 +1,7 @@
 function run_multiple_HBTF(cm)
 	disp('USING..........')
 	disp(cm)
+	cm = str2num(cm);
 	% Load in the empirical SC matrix
 	load('empirical_data/exemplarSC.mat')
 	addpath(genpath('~/projects/bdtoolkit')); % Add the bdtoolkit
@@ -9,7 +10,7 @@ function run_multiple_HBTF(cm)
 	% Do some post-processing of the SCM:
 	C = C/max(C(:));
 	C = C - diag(diag(C));
-
+	disp(C)
 	% After doing this run the BTF model for parameter
 
 	% A really simple way to do this..
