b80c3add477ac0502f8929e788e362c8ac22cd15
Kevin Aquino <kevin.aquino@monash.edu>
2019-07-22 21:20:17 +1000

    Pre allocation makes the code 100 times faster in one part and 20 times in antoher,  its much faster now.

--- models/run_hopf_model_heterogenous_edge_bif.m
+++ models/run_hopf_model_heterogenous_edge_bif.m
@@ -1,4 +1,4 @@
-function [ts_simulated_all,grandFCcorr,FCD,all_anecs] = run_hopf_model_heterogenous_edge_bif(sc_matrix,time_series,G,folder)
+function [ts_simulated_all,grandFCcorr,FCD,all_anecs,all_MSE] = run_hopf_model_heterogenous_edge_bif(sc_matrix,time_series,G,folder)
 
 % Setting up the parameters
 C = sc_matrix;
@@ -40,19 +40,29 @@ sig=0.02; % This sigma is used to determine the strength of noise in the Hopf mo
 WE=G;
 a=-0.01*ones(N,2);
 Cnew=C;
-ITER=1:50;
+ITER=1:20;
 
 Tmax=Tmax*NSUB;
 
 iwe=1;
 for we=WE,
-	disp(we);
+    figure;
+    subplot(1,2,1);
+    imagesc(C);
+    title('DATA SC');
+    axis image;
+    disp(['G=',num2str(we)]);
+	% disp(we);
     Cnew=C;
     % Could possibly use parfor here for parallel processing.
+    MSE=100;    
+    MSE_run = [];
     for iter=ITER
+        disp(['ITERATION_=',num2str(iter),' MSE=',num2str(MSE)]);
         wC = we*Cnew;
         sumC = repmat(sum(wC,2),1,2); % for sum Cij*xj
         if length(ITER)>1
+            % keyboard
         	% Solve the hopf ode/sde:
 
         	% Solve the HOPF model here.
@@ -62,6 +72,7 @@ for we=WE,
             %%%%
             BOLD=xs';
             % Now calculate the instantenous FC using the phase type of analysis
+            % keyboard
             iFCsim = calculate_iFC(BOLD,TR);
 
             FCphases=squeeze(mean(iFCsim));
@@ -81,8 +92,9 @@ for we=WE,
             
             Cnew=Cnew/max(max(Cnew))*0.2;
             
-            D = abs(FCphasesemp-FCphases).^2;
+            D = abs(FCphasesemp-FCphases).^2;            
             MSE = sum(D(:))/numel(FCphases);
+            MSE_run = [MSE_run,MSE];
             if MSE<0.01
                 break;
             end
@@ -110,10 +122,18 @@ for we=WE,
 
     % Calculate the FCD based on the phases and compare this to the simulation using a KS test implemented in matlab   
     % Note for consistency this is now calculated in a universal function.
+    % keyboard
 	phfcd = phase_fcd(xs,TR);
     [H,P,FCD(iwe)]=kstest2(phfcd,phfcddata);       
 
     all_anecs{iwe}=Cnew;
+    all_MSE{iwe} = MSE_run;
+
+    subplot(1,2,2);
+    imagesc(Cnew);
+    title(['ANEC, G =',num2str(we)]);
+    axis image;
+    drawnow;
     iwe=iwe+1;
 
 end
@@ -122,6 +142,7 @@ end
 
 % A sub-function to calculate instantneous FC
 function iFC = calculate_iFC(time_series,TR)
+    % keyboard
 	delt = TR;            % sampling interval
 	k=2;                  % 2nd order butterworth filter
 	fnq=1/(2*delt);
@@ -133,12 +154,15 @@ function iFC = calculate_iFC(time_series,TR)
 
 	signaldata=squeeze(time_series);  %%seed, time, sub    
     Phase_BOLD_data=zeros(N,Tmax);
+    % keyboard
     for seed=1:N
         signaldata(seed,:)=signaldata(seed,:)-mean(signaldata(seed,:));
         signal_filt_data =filtfilt(bfilt2,afilt2,signaldata(seed,:));
         Phase_BOLD_data(seed,:) = angle(hilbert(signal_filt_data));
     end
-    
+    % Pre-allocation makes this 100 times faster
+    iFC=zeros(Tmax,N,N);
+
     for t=1:Tmax
         for n=1:N
             for p=1:N
