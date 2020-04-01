8e4cdbbc10b0a0ba8dbc240391dc2362de9e55c1
Kevin Aquino <kevin.aquino@monash.edu>
2020-04-01 15:51:07 +1100

    updated to run more finely

--- models/test_run_hopf_no_ts.m
+++ models/test_run_hopf_no_ts.m
@@ -1,27 +1,28 @@
-% Structural connectivity test:
-
-% First find the optimal parameters
-
-
-
-load('empirical_data/SC_HCP_cortexOnly.mat')
+% HOPF Model without data:
+% 
 % SC Matrix normalization
 C=AdjDens;
 C = C/max(C(:))*0.2;
 
-% Frequency specification for each node (its realtively )
+% Frequency specification for each node (its relatively constant for all time series)
 f_diff=ones(N,1)*0.05;
 TR=0.72;
 
 % Global coupling parameter (tune this)
 G = 5;
 % Frequency of each node:
-
 xs = run_hopf_no_ts(C,G,f_diff,TR);
 
 
+% Calculating Hopf model for the given data, to work out 
+G_vec = linspace(0,10,40);
+[ts_simulated_all,grandFCcorr,bifpar,FCD] = run_hopf_model_homogenous_bif(C,all_time_series,G_vec,'',TR);
 
-imagesc(corr(xs));
-
-G_vec = linspace(0,10,20);
-[ts_simulated_all,grandFCcorr,bifpar,FCD] = run_hopf_model_homogenous_bif(C,ts_all,G_vec,'',TR);
+figure('color','white');
+plot(G_vec,grandFCcorr,'r.-','lineWidth',2,'MarkerSize',30);
+hold on;
+plot(G_vec,FCD,'k.-','lineWidth',2,'MarkerSize',30);
+title('Looking for G to max. FCorr, min. FCD');
+xlabel('G')
+ylabel('FCD (black),FCorr (red)')
+set(gca,'fontSize',18);
