7f1a1effc91679360db47273ea6c33843a14ee84
Kevin Aquino <kevin.aquino@monash.edu>
2020-06-16 15:17:18 +1000

    Fixing up code for the cluster runs

--- batch_btf_cluster.sh
+++ batch_btf_cluster.sh
@@ -1,14 +1,14 @@
 # !/bin/bash
 
 # This script is to set it all up (running on matlab 2018b)
-module load matlab/r2018a
+# module load matlab/r2018a
 for g in `seq 1 20`;
 	do
 		for r in `seq 1 108`;
 			do
 				export G_ind=$g
 				export Run=$r
-				echo "G index: " $G_ind "Run index:" $Run
-				sbatch --job-name="BTF_${G_ind}" batch_btf_slurm_job.sh
+				echo $G_ind","$Run
+				# sbatch --job-name="BTF_${G_ind}" batch_btf_slurm_job.sh
 			done
     done
--- batch_btf_slurm_job.sh
+++ batch_btf_slurm_job.sh
@@ -7,7 +7,20 @@
 #SBATCH --export=ALL
 #SBATCH --mem-per-cpu=24000
 #SBATCH -A kg98
+#SBATCH --array=1-2160
 
-echo $G_ind $Run
+FILE='BTF_batch_list.txt'
+
+BATCH_STRING=$(sed -n "${SLURM_ARRAY_TASK_ID}p" ${FILE})
+
+G_ind=$(echo $BATCH_STRING | awk -F, '{print $1}')
+Run=$(echo $BATCH_STRING | awk -F, '{print $2}')
+
+
+echo -e "\t\t\t --------------------------- "
+echo -e "\t\t\t ----- ${SLURM_ARRAY_TASK_ID} ${subject} ----- "
+echo -e "\t\t\t --------------------------- \n"
+
+
 
 matlab -nodisplay -r "setup_model_comparisons;batch_BTF_run('${G_ind}','${Run}'); exit"
