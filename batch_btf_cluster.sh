4b2cbd71aa84b7dbcd65afe64b6a4d9b713218bf
Kevin Aquino <kevin.aquino@monash.edu>
2020-06-16 21:37:00 +1000

    Adding a better way to send out jobs

--- batch_btf_cluster.sh
+++ batch_btf_cluster.sh
@@ -1,14 +1,16 @@
 # !/bin/bash
 
 # This script is to set it all up (running on matlab 2018b)
-# module load matlab/r2018a
+module load matlab/r2018a
+
+# Now run this for each G value
+
+
 for g in `seq 1 20`;
 	do
-		for r in `seq 1 108`;
-			do
-				export G_ind=$g
-				export Run=$r
-				echo $G_ind","$Run
-				# sbatch --job-name="BTF_${G_ind}" batch_btf_slurm_job.sh
-			done
+		export G_ind=$g		
+		echo "Sending G_ind: "$G_ind "To Massive"
+		sbatch --job-name="BTF_"$G_ind batch_btf_slurm_job.sh
     done
+
+
--- batch_btf_slurm_job.sh
+++ batch_btf_slurm_job.sh
@@ -1,6 +1,5 @@
 #!/bin/env bash
 
-#SBATCH --job-name=BTF
 #SBATCH --account=kg98
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
@@ -8,22 +7,17 @@
 #SBATCH --export=ALL
 #SBATCH --mem-per-cpu=24000
 #SBATCH -A kg98
-#SBATCH --array=1-999
+#SBATCH --array=1-108
 #SBATCH --mail-user=kevin.aquino@monash.edu
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-type=END
 
 
-FILE='BTF_batch_list.txt'
-
-BATCH_STRING=$(sed -n "${SLURM_ARRAY_TASK_ID}p" ${FILE})
-
-G_ind=$(echo $BATCH_STRING | awk -F, '{print $1}')
-Run=$(echo $BATCH_STRING | awk -F, '{print $2}')
-
+# Note: G_ind is passed on through another script.
+Run=${SLURM_ARRAY_TASK_ID}
 
 echo -e "\t\t\t --------------------------- "
-echo -e "\t\t\t ----- ${SLURM_ARRAY_TASK_ID} ${subject} ----- "
+echo -e "\t\t\t ----- ${SLURM_ARRAY_TASK_ID} / 108 For $G_ind ----- "
 echo -e "\t\t\t --------------------------- \n"
 
 
