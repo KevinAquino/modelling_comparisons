6002067fac709e12c886a3b47741100cba9ea418
Kevin Aquino <kevin.aquino@monash.edu>
2020-06-19 10:03:28 +1000

    Changed this to worth with multiple models

--- simulate_heterogenous_node.m
+++ simulate_heterogenous_node.m
@@ -16,3 +16,22 @@ for nm=1:length(MODELS),
 	run_network_model(empirical_params,simulation_params);
 end
 
+% Copy these results into another directory
+
+!cp -r UCLA/results/HOPF+HETEROGENOUS UCLA/results/HOPF+HETEROGENOUS+GSR
+
+% Now try do the same analysis with DiCER and not GSR'ing the model as well
+
+simulation_params.preproMethod=3;
+simulation_params.UseGSR=0;
+
+for nm=1:length(MODELS),
+	% Run the models:
+	model_time_series=permute(time_series(:,:,:,:),[2 1 3 4]);
+	simulation_params.MODEL=MODELS{nm}
+	% Note: the Hopf Model needs a description of the mean w_f, and the NDM needs a time series
+	% They are not nescessary in detail - but calculated here based on time series.	
+	run_network_model(empirical_params,simulation_params);
+end
+
+!cp -r UCLA/results/HOPF+HETEROGENOUS UCLA/results/HOPF+HETEROGENOUS+DICER
